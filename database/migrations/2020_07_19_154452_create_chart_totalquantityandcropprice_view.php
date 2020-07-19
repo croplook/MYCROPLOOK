9f5d6b39bcfc1e0a62c0b364fe4e7952270eb50c
Amilissa Araneta <araamilissa@gmail.com>
2020-07-20 03:44:20 +0800

    changed data type for pgsql in heroku with ::decimal part 5

--- database/migrations/2020_07_19_154452_create_chart_totalquantityandcropprice_view.php
+++ database/migrations/2020_07_19_154452_create_chart_totalquantityandcropprice_view.php
@@ -17,16 +17,16 @@ class CreateChartTotalquantityandcroppriceView extends Migration
         CREATE VIEW chart_totalquantityandcropprice
         AS
         SELECT 
+        posts.crop_name AS crop_name,
         posts.id AS id,
         posts.user_id AS user_id,
         SUM(posts.crop_quantity::decimal) AS sumCropQty,
         (SUM(posts.crop_quantity::decimal) * posts.crop_price::decimal) AS allCropPrice,
         AVG(posts.crop_price::decimal) AS avgCropPrice,
-        posts.crop_name AS crop_name,
         posts.created_at AS created_at
         FROM
         posts
-        GROUP BY posts.crop_name
+        GROUP BY posts.crop_name, posts.id 
         ");
     }
 
