87edcaf90ff8ee32de97ad051c1a2c764a7b089b
Amilissa Araneta <araamilissa@gmail.com>
2020-07-20 02:42:30 +0800

    changed data type for pgsql in heroku part 3

--- database/migrations/2020_07_19_154452_create_chart_totalquantityandcropprice_view.php
+++ database/migrations/2020_07_19_154452_create_chart_totalquantityandcropprice_view.php
@@ -20,7 +20,7 @@ class CreateChartTotalquantityandcroppriceView extends Migration
         posts.id AS id,
         posts.user_id AS user_id,
         SUM(posts.crop_quantity) AS sumCropQty,
-        (posts.crop_price * SUM(posts.crop_quantity)) AS allCropPrice,
+        SUM(posts.crop_quantity * posts.crop_price) AS allCropPrice,
         AVG(posts.crop_price) AS avgCropPrice,
         posts.crop_name AS crop_name,
         posts.created_at AS created_at
